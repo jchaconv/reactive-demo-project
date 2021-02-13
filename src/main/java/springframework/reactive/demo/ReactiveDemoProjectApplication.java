2c06bea7c10c6a0742803fd469de45600075e850
jchaconv <jcaaron.chacon@outlook.com>
2021-02-12 20:53:43 -0500

    Added files to the project.

---
+++ src/main/java/springframework/reactive/demo/ReactiveDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package springframework.reactive.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springframework/reactive/demo/ReactiveDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.reactive.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
